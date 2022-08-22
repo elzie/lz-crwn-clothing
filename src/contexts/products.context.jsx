cf30cf756ff7703103e17fcba9ab711355937085
Djan K. Blom <djan@live.dk>
2022-08-22 17:29:37 +0200

    stored data into firebase via. batch, and retrieved them again

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../utils/firebase.utils';
 
-import PRODUCTS from '../shop-data.json';
+// import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
   // store an array of products
@@ -8,7 +9,24 @@ export const ProductsContext = createContext({
   // some function that allows us to set those products.
 });
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
+  // add JSON object to the db.
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, []);
+  // only run this once, ever.
+
+  useEffect(() => {
+    // any async things needed in useEffect, wrap it in an async-function.
+    const getCategoriesMap = async () => {
+      const categoriesMap = await getCategoriesAndDocuments();
+      console.log(categoriesMap);
+    };
+    // invoke the method
+    getCategoriesMap();
+  }, []);
+
   //export out the value, which is the products
   const value = { products };
   return (
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -9,7 +9,16 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query,
+  getDocs,
+} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -38,6 +47,46 @@ export const signInWithGoogleRedirect = () =>
 
 export const db = getFirestore();
 
+/**
+ * creating collections and documents using batch
+ *
+ * collectionKey is the name of the collection in the db
+ * objectsToAdd is the document-name
+ */
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  // Collection Reference
+  const collectionRef = collection(db, collectionKey);
+
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log('batch done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories');
+
+  const q = query(collectionRef);
+
+  const querySnapshot = await getDocs(q);
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    // destructure off the values
+    const { title, items } = docSnapshot.data();
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+  // Google changes this shit in every update ^^'... but it is backwards compatible.
+  return categoryMap;
+};
+
 // create user in database
 export const createUserDocumentFromAuth = async (
   userAuth,
@@ -47,11 +96,11 @@ export const createUserDocumentFromAuth = async (
 
   const userDocRef = doc(db, 'users', userAuth.uid);
 
-  console.log('a', userDocRef);
+  // console.log('a', userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
 
-  console.log('b', userSnapshot);
+  // console.log('b', userSnapshot);
 
   if (!userSnapshot.exists()) {
     // if user data does not exist
